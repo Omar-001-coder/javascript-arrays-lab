8034bc218f137840c6008f5998c9004467450e06
omar <khadijahyasmin8@gmail.com>
2023-03-03 11:56:40 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-var kittens = [] //define your array here
+// Write your solution here!
+const kittens=['Milo', 'Otis', 'Garfield'];
 
-// Add your functions and code here
+
+function destructivelyAppendKitten(name){
+    kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+    kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+    kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+    kittens.shift();
+}
+
+function appendKitten(name){
+  return kittens.concat(name);
+}
+
+function prependKitten(name){
+  const newKitten=[name, ...kittens];
+  return newKitten;
+}
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1,kittens.length);
+}
